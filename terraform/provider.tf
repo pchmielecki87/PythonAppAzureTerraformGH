ee11d4b078611c4398f7a0de1d4bf4fd0af42edb
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2025-12-29 08:51:06 +0100

    use new container in SA for GHA statefile

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,7 +8,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "python-terraform-ado"
     storage_account_name = "tfstateforpythonapp"
-    container_name       = "tfstate"
+    container_name       = "ghatfstate"
     key                  = "terraform.tfstate"
   }
 }
