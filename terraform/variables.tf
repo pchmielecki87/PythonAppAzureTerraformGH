1d2a2c4d457cb358e43f880ac24a924aaeb6edfc
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2025-12-31 15:46:47 +0100

    change validation to allow 5-15 chars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,8 +15,8 @@ variable "prefix" {
   default = null
 
   validation {
-    condition     = var.prefix == null || can(regex("^[a-z0-9-]{2,10}$", var.prefix))
-    error_message = "prefix must be 2–10 characters, lowercase letters, numbers or hyphens."
+    condition     = var.prefix == null || can(regex("^[a-z0-9-]{5,15}$", var.prefix))
+    error_message = "prefix must be 5–15 characters, lowercase letters, numbers or hyphens."
   }
 }
 
