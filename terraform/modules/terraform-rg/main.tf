2600c05f2d6d9b951c2c2c2d4dc0edfa6b5f6570
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2026-01-02 07:29:07 +0100

    add tf version and provider for linter

--- terraform/modules/terraform-rg/main.tf
+++ terraform/modules/terraform-rg/main.tf
@@ -1,3 +1,21 @@
+# Copyright 2025 TechBrain. All rights reserverd.
+#
+# Created on: 2025.12.01
+# Created by: Przemyslaw Chmielecki
+# Modified on: 2026.01.02
+# Modified by: Przemyslaw Chmielecki
+
+# TF and Provider version - needed for TFLint
+terraform {
+  required_version = ">= 1.5" # TF version
+  required_providers {        # Azure RM provider version
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.57"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.location
