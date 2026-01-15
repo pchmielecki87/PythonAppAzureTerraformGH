9654a4014e1a2a34fc5b189279e106a98f50e660
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2026-01-15 20:36:36 +0100

    revert module for asp

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,73 +2,57 @@
 #
 # Created on: 2025.12.01
 # Created by: Przemyslaw Chmielecki
-# Modified on: 2026.01.08
+# Modified on: 2026.01.02
 # Modified by: Przemyslaw Chmielecki
 
+#############################################################################
+## SINGLE MAIN.TF WITH ALL RESOURCES, NO MODULES
+#############################################################################
+
 ## RG #######################################################################
-module "rg" {
-  source   = "./modules/terraform-rg"
-  rg_name  = var.rg_name
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
   location = var.location
   tags     = var.tags
 }
 
 ## WEBAPP ##################################################################
-module "asp" {
-  source                = "./modules/terraform-asp"
-  prefix                = var.prefix
-  app_service_plan_name = "${var.prefix}-${var.app_service_plan_name}"
-  rg_name               = var.rg_name
-  location              = var.location
-  app_service_name      = "${var.prefix}-${var.app_service_name}"
-  os_type               = var.os_type
-  sku_name              = var.sku_name
-  tags                  = var.tags
-
-  depends_on = [module.rg]
+resource "azurerm_service_plan" "asp" {
+  name                = "${var.prefix}-asp"
+  location            = var.location
+  resource_group_name = var.rg_name # normally I could add indirect reference module.rg.name instead of var.rg_name but it WILL NOT use tfvars then
+  os_type             = var.os_type
+  sku_name            = var.sku_name
 }
 
-module "appservice" {
-  source                = "./modules/terraform-appservice"
-  prefix                = var.prefix
-  rg_name               = var.rg_name
-  app_service_plan_name = "${var.prefix}-${var.app_service_plan_name}"
-  app_service_name      = "${var.prefix}-${var.app_service_name}"
-  os_type               = var.os_type
-  sku_name              = var.sku_name
-  tags                  = var.tags
+resource "azurerm_linux_web_app" "app" {
+  name                = "${var.prefix}-app"
+  location            = var.location
+  resource_group_name = var.rg_name
+  service_plan_id     = azurerm_service_plan.asp.id
+  https_only          = true
 
-  depends_on = [module.asp, azurerm_application_insights.ai]
+  site_config {
+    always_on = false
+    application_stack {
+      python_version = "3.11"
+    }
+  }
+
+  app_settings = {
+    "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.ai.connection_string
+    "WEBSITE_RUN_FROM_PACKAGE"              = "1"
+    "APPINSIGHTS_INSTRUMENTATIONKEY"        = azurerm_application_insights.ai.instrumentation_key
+    # "APPLICATIONINSIGHTS_CONNECTION_STRING" = "InstrumentationKey=${azurerm_application_insights.ai.instrumentation_key}"
+    "PYTHON_VERSION"                 = "3.11"
+    "KEYVAULT_URI"                   = data.azurerm_key_vault.kv.vault_uri
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = true
+  }
+
+  tags = var.tags
+
+  depends_on = [azurerm_service_plan.asp, azurerm_application_insights.ai]
 }
-
-# resource "azurerm_linux_web_app" "app" {
-#   name                = "${var.prefix}-${var.app_service_name}"
-#   location            = var.location
-#   resource_group_name = var.rg_name
-#   service_plan_id     = module.asp.app_service_plan_id # get it from output inside ASP module
-#   https_only          = true
-
-#   site_config {
-#     always_on = false
-#     application_stack {
-#       python_version = "3.11"
-#     }
-#   }
-
-#   app_settings = {
-#     "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.ai.connection_string
-#     "WEBSITE_RUN_FROM_PACKAGE"              = "1"
-#     "APPINSIGHTS_INSTRUMENTATIONKEY"        = azurerm_application_insights.ai.instrumentation_key
-#     # "APPLICATIONINSIGHTS_CONNECTION_STRING" = "InstrumentationKey=${azurerm_application_insights.ai.instrumentation_key}"
-#     "PYTHON_VERSION"                 = "3.11"
-#     "KEYVAULT_URI"                   = data.azurerm_key_vault.kv.vault_uri
-#     "SCM_DO_BUILD_DURING_DEPLOYMENT" = true
-#   }
-
-#   tags = var.tags
-
-#   depends_on = [module.asp, azurerm_application_insights.ai]
-# }
 
 ## LAW ####################################################################
 resource "azurerm_log_analytics_workspace" "law" {
